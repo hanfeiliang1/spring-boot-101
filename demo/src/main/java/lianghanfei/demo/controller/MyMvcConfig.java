3aa43a2ebfafb5e2fb590835a95b42025402dd7e
liang <hanfeiliang1031@outlook.com>
2020-12-29 09:51:45 +0800

    update

---
+++ demo/src/main/java/lianghanfei/demo/controller/MyMvcConfig.java
@@ -0,0 +1,14 @@
+package sample.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+
+public class MyMvcConfig implements WebMvcConfigurer {
+
+
+
+}
